6b2d3befa8502908ade009824dffa15eca36ff3b
kado1130 <kadoros1130@gmail.com>
2024-06-23 03:17:28 +0200

    before #6.11 Add To Do

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,4 +1,15 @@
+import { useForm } from "react-hook-form";
+
 function ToDoList() {
-    return <div>ToDoList</div>;
+  const { register } = useForm();
+  return (
+    <div>
+      <form style={{ display: "flex", flexDirection: "column" }}>
+        <input {...register("todo")} placeholder="write a to do" />
+        <input {...register("qwe")} placeholder="qwe" />
+        <button>Add</button>
+      </form>
+    </div>
+  );
 }
 export default ToDoList;
