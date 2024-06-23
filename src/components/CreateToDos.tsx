77b76fecd02e055b934022daca68418e16dd1224
kado1130 <kadoros1130@gmail.com>
2024-06-23 04:34:35 +0200

    #6.15 Immutability part Two: button func.

---
+++ src/components/CreateToDos.tsx
@@ -0,0 +1,28 @@
+import { useForm } from "react-hook-form";
+import { useSetRecoilState } from "recoil";
+import { toDoState } from "../atoms";
+
+interface IForm {
+  toDo: string;
+}
+
+function CreateToDos() {
+  const setToDos = useSetRecoilState(toDoState);
+  const { register, handleSubmit, setValue } = useForm<IForm>();
+
+  const onValid = ({ toDo }: IForm) => {
+    setToDos((oldToDos) => [
+      { text: toDo, id: Date.now(), category: "TO_DO" },
+      ...oldToDos,
+    ]);
+    setValue("toDo", "");
+  };
+  return (
+    <form onSubmit={handleSubmit(onValid)}>
+      <input {...register("toDo")} placeholder="write a to do" />
+      <button>Add</button>
+    </form>
+  );
+}
+
+export default CreateToDos;
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,43 +1,18 @@
-import { useForm } from "react-hook-form";
-import { atom, useRecoilState } from "recoil";
-
-const toDoState = atom<IToDo[]>({
-  key: "toDo",
-  default: [],
-});
-
-interface IForm {
-  toDo: string;
-}
-
-interface IToDo {
-  text: string;
-  id: number;
-  category: "TO_DO" | "DOING" | "DONE";
-}
+import { useRecoilValue } from "recoil";
+import { toDoState } from "../atoms";
+import CreateToDos from "./CreateToDos";
+import ToDo from "./ToDo";
 
 function ToDoList() {
-  const [toDos, setToDos] = useRecoilState(toDoState);
-  const { register, handleSubmit, setValue } = useForm<IForm>();
-
-  const onValid = ({ toDo }: IForm) => {
-    setToDos((oldToDos) => [{ text: toDo, id: Date.now(), category: "TO_DO" }, ...oldToDos]);
-    setValue("toDo", "");
-  };
-
-  console.log(toDos);
-
+  const toDos = useRecoilValue(toDoState);
   return (
     <div>
       <h1>To Do List</h1>
       <hr />
-      <form onSubmit={handleSubmit(onValid)}>
-        <input {...register("toDo")} placeholder="write a to do" />
-        <button>Add</button>
-      </form>
+      <CreateToDos />
       <ul>
-        {toDos.map((toDo, index) => (
-          <li key={index}>{toDo.text}</li>
+        {toDos.map((toDo) => (
+          <ToDo {...toDo} />
         ))}
       </ul>
     </div>
