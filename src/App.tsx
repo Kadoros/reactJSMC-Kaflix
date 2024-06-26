7a580d43ce29f955a34069756eedd7d4efb84d49
kado1130 <kadoros1130@gmail.com>
2024-06-27 01:38:25 +0200

    #8.4 Variants part Two

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import { delay, motion } from "framer-motion";
 import { start } from "repl";
 
 const Wrapper = styled.div`
@@ -13,26 +13,63 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
-  border-radius: 10px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const myVariant = {
+const Circle = styled(motion.div)`
+  width: 70px;
+  height: 70px;
+  place-self: center;
+  border-radius: 35px;
+  background-color: white;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVariants = {
   start: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  end: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      type: "spring",
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.2,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const circleVariants = {
+  start: {
+    opacity: 0,
     scale: 0,
   },
   end: {
+    opacity: 1,
     scale: 1,
-    rotateZ: 360,
-    transition: { type: "spring", duration: 1, delay: 1 },
+    transition: {
+      type: "spring",
+    },
   },
 };
 
 function App() {
   return (
     <Wrapper>
-      <Box variants={myVariant} initial="start" animate="end"/>
+      <Box initial="start" animate="end" variants={boxVariants}>
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+      </Box>
     </Wrapper>
   );
 }
