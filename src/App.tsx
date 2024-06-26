6cdc940d75e0869757ba8788ed58717a6421674f
kado1130 <kadoros1130@gmail.com>
2024-06-27 01:08:15 +0200

    #8.3 Variants part One

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { motion } from "framer-motion";
+import { start } from "repl";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -8,7 +10,7 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Box = styled.div`
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
@@ -16,10 +18,21 @@ const Box = styled.div`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const myVariant = {
+  start: {
+    scale: 0,
+  },
+  end: {
+    scale: 1,
+    rotateZ: 360,
+    transition: { type: "spring", duration: 1, delay: 1 },
+  },
+};
+
 function App() {
   return (
     <Wrapper>
-      <Box />
+      <Box variants={myVariant} initial="start" animate="end"/>
     </Wrapper>
   );
 }
