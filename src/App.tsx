5416ae37d8f8540a467896d8d5707fb1cdbd4f1e
kado1130 <kadoros1130@gmail.com>
2024-06-29 17:14:35 +0200

    #8.12 Slider part One

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,70 @@
 import styled from "styled-components";
-import { motion, useMotionValue, useTransform, useScroll  } from "framer-motion";
-
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
 const Wrapper = styled(motion.div)`
-  height: 500vh;
+  height: 100vh;
   width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
-  background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
+  flex-direction: column;
 `;
 
 const Box = styled(motion.div)`
-  width: 200px;
+  width: 400px;
   height: 200px;
-  display: grid;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
+  position: absolute;
+  top: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-function App() {
-  const x = useMotionValue(0);
-  const rotateZ = useTransform(x, [-200, 200], [-360, 360]);
-  const gradientColor = useTransform(
-    x,
-    [-200, 0, 200],
-    [
-      "linear-gradient(135deg, rgb(0, 210, 238), rgb(10, 0, 10))",
-      "linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238))",
-      "linear-gradient(135deg, rgb(100, 20, 2), rgb(30, 100, 100))",
-    ]
-  );
+const BoxVariants = {
+  invisible: {
+    x: 500,
+    opacity: 0,
+    scale: 0,
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    x: 0,
+  },
+  exit: {
+    x: -500,
+    opacity: 0,
+    scale: 0,
+  },
+};
 
-  const {scrollYProgress} = useScroll ();
-  const scale = useTransform(scrollYProgress, [0, 0.5, 1], [0.5, 1, 2]);
+function App() {
+  const [visible, setvisible] = useState(1);
+  const next = () => {
+    setvisible((prev) => (prev === 10 ? 10 : prev + 1));
+  };
   return (
-    <Wrapper style={{ background: gradientColor }}>
-      <Box style={{ x, rotateZ, scale}} drag="x" dragSnapToOrigin />
+    <Wrapper>
+      <AnimatePresence>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) =>
+          i === visible ? (
+            <Box
+              key={i}
+              variants={BoxVariants}
+              initial="invisible"
+              animate="visible"
+              exit="exit"
+            >
+              {i}
+            </Box>
+          ) : null
+        )}
+      </AnimatePresence>
+      <button onClick={next}>Next</button>
     </Wrapper>
   );
 }
