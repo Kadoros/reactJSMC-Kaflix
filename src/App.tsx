d5c4357e2201c3a508bd79f2da153d70e1a9c41c
kado1130 <kadoros1130@gmail.com>
2024-06-29 04:07:39 +0200

    #8.6 Gestures part Two

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
-import { delay, motion } from "framer-motion";
+import { circIn, delay, motion } from "framer-motion";
 import { start } from "repl";
+import { useRef } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -10,66 +11,50 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  overflow: hidden;
+`;
+
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-  width: 70px;
-  height: 70px;
-  place-self: center;
-  border-radius: 35px;
-  background-color: white;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
 const boxVariants = {
-  start: {
-    opacity: 0,
-    scale: 0.5,
-  },
-  end: {
-    opacity: 1,
-    scale: 1,
-    transition: {
-      type: "spring",
-      duration: 0.5,
-      bounce: 0.5,
-      delayChildren: 0.2,
-      staggerChildren: 0.2,
-    },
-  },
-};
-
-const circleVariants = {
-  start: {
-    opacity: 0,
-    scale: 0,
-  },
-  end: {
-    opacity: 1,
-    scale: 1,
-    transition: {
-      type: "spring",
-    },
-  },
+  hover: { scale: 1.5, rotateZ: 90 },
+  tap: { scale: 0.8, borderRadius: "100px" },
+  drag: { backgroundColor: "rgb(46,204,113)" },
 };
 
 function App() {
+  const BiggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <Box initial="start" animate="end" variants={boxVariants}>
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-      </Box>
+      <BiggerBox ref={BiggerBoxRef}>
+        <Box
+          drag
+          dragElastic={0.1}
+          dragSnapToOrigin
+          dragConstraints={BiggerBoxRef}
+          variants={boxVariants}
+          whileHover="hover"
+          whileDrag="drag"
+          whileTap="tap"
+        />
+      </BiggerBox>
     </Wrapper>
   );
 }
